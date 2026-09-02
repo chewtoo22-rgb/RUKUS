da48b254fc5566332e7730248e8c00b137235789
Matt H <chewtoo22@gmail.com>
2026-09-02 15:01:20 -0400

    QA: cover bounded telemetry event boundary

---
+++ app/src/test/java/com/ruckus/agent/telemetry/TelemetryEventBoundaryTest.kt
@@ -0,0 +1,52 @@
+package com.ruckus.agent.telemetry
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TelemetryEventBoundaryTest {
+    @Test
+    fun preserves_allowlisted_event_and_attributes() {
+        val event = TelemetryEvent(
+            name = TelemetryEvent.Name.COMMAND_FINISHED,
+            timestampElapsedRealtimeMs = 42,
+            attributes = mapOf("command_id" to "abc", "result" to "success"),
+        )
+
+        assertEquals(event, TelemetryEventBoundary.sanitize(event))
+    }
+
+    @Test
+    fun rejects_negative_timestamp() {
+        assertFailsWith<IllegalArgumentException> {
+            TelemetryEventBoundary.sanitize(
+                TelemetryEvent(TelemetryEvent.Name.COMMAND_STARTED, -1),
+            )
+        }
+    }
+
+    @Test
+    fun rejects_oversized_attribute_value() {
+        assertFailsWith<IllegalArgumentException> {
+            TelemetryEventBoundary.sanitize(
+                TelemetryEvent(
+                    name = TelemetryEvent.Name.SETTING_CHANGED,
+                    timestampElapsedRealtimeMs = 1,
+                    attributes = mapOf("setting" to "x".repeat(TelemetryEventBoundary.MAX_VALUE_LENGTH + 1)),
+                ),
+            )
+        }
+    }
+
+    @Test
+    fun rejects_attribute_count_overflow() {
+        val attributes = (0..TelemetryEventBoundary.MAX_ATTRIBUTE_COUNT)
+            .associate { "key_$it" to "value" }
+
+        assertFailsWith<IllegalArgumentException> {
+            TelemetryEventBoundary.sanitize(
+                TelemetryEvent(TelemetryEvent.Name.PERMISSION_CHECKED, 1, attributes),
+            )
+        }
+    }
+}
